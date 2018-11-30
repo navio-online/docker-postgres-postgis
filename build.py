2612eb006a412c2bd3af97ba6ebba57905d03838
Peter Salnikov <peter@salnikov.cc>
2018-11-30 18:22:05 +0300

    add ci

--- build.py
+++ build.py
@@ -1,7 +1,37 @@
 from navio.builder import task, nsh, sh
+from navio.travis import Travis
 
+travis = Travis().is_travis()
+branch = Travis().branch()
+
+Config = {
+  'master': {
+    'docker_tag': 'latest',
+  },
+}
 
 @task()
-def build():
-  nsh.docker('build --rm=true -t postgis .'.split(' '))
+def setup_docker():
+  nsh.docker('login', '-u', os.environ.get('DOCKER_HUB_USER'), '-p', os.environ.get('DOCKER_HUB_PASS'))
 
+@task(setup_docker)
+def build():
+  nsh.docker('build --rm=true -t alpine-postgres-postgis .'.split(' '))
+
+@task(build)
+def push():
+  if os.environ.get('TRAVIS_PULL_REQUEST', 'false') == 'true':
+    print("Skipping docker image push for push requests")
+    return
+
+  if branch in Config:
+    nsh.docker.tag(
+        'alpine-postgres-postgis:latest', 
+        'navioonline/alpine-postgres-postgis:{tag}'.format(Config[branch]['docker_tag'])
+      )
+
+  if not Travis().is_pull_request() and Travis().is_tag():
+    nsh.docker.tag(
+        'alpine-postgres-postgis:latest', 
+        'navioonline/alpine-postgres-postgis:{tag}'.format(Travis().tag())
+      )
