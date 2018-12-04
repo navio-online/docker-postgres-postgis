834bc99e0a532375b563a0697704a1117ed5bfe9
Peter Salnikov <peter@salnikov.cc>
2018-12-04 17:08:31 +0500

    fixes

--- files/docker-entrypoint-initdb.d/initdb-postgis.sh
+++ files/docker-entrypoint-initdb.d/initdb-postgis.sh
@@ -21,6 +21,5 @@ for DB in template_postgis "$POSTGRES_DB"; do
     CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
     CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
     CREATE EXTENSION IF NOT EXISTS address_standardizer;
-    CREATE EXTENSION IF NOT EXISTS address_standardizer_data_us;    
 EOSQL
 done
